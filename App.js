939c5e4f96d9d633bfb41f899ecbfe2ee63eeef3
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2022-10-11 09:44:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,57 +1,53 @@
-const canvas = document.getElementById('drawing-board');
-const toolbar = document.getElementById('toolbar');
-const ctx = canvas.getContext('2d');
+// get input values
+let bill = document.querySelector('#bill');
+let people = document.querySelector('#numOfPeople');
+let custom = document.querySelector('#custom');
+let tipAmount = 0;
 
-const canvasOffsetX = canvas.offsetLeft;
-const canvasOffsetY = canvas.offsetTop;
-
-canvas.width = window.innerWidth - canvasOffsetX;
-canvas.height = window.innerHeight - canvasOffsetY;
-
-let isPainting = false;
-let lineWidth = 5;
-let startX;
-let startY;
-
-toolbar.addEventListener('click', e => {
-    if (e.target.id === 'clear') {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-});
-
-toolbar.addEventListener('change', e => {
-    if(e.target.id === 'stroke') {
-        ctx.strokeStyle = e.target.value;
-    }
-
-    if(e.target.id === 'lineWidth') {
-        lineWidth = e.target.value;
-    }
+const reset = document.querySelector('.reset');
+      tip = document.querySelector('.tip');
+      five = document.querySelector('.five');
+      ten = document.querySelector('.ten');
+      fifteen = document.querySelector('.fifteen');
+      twentyFive = document.querySelector('.twentyFive');
+      fifty = document.querySelector('.fifty');
+  
     
-});
-
-const draw = (e) => {
-    if(!isPainting) {
-        return;
-    }
-
-    ctx.lineWidth = lineWidth;
-    ctx.lineCap = 'round';
-
-    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
-    ctx.stroke();
+// get custom tip
+function getTipCustom() {
+  tipAmount = parseInt(custom.value, 10);
 }
 
-canvas.addEventListener('mousedown', (e) => {
-    isPainting = true;
-    startX = e.clientX;
-    startY = e.clientY;
+// get tip
+document.querySelector('.tip-container').addEventListener('click', event => {
+  if (event.target !== five && event.target !== ten && event.target !== fifteen && event.target !== twentyFive && event.target !== fifty) {
+    return
+  }
+  tipAmount = event.target.value 
 });
 
-canvas.addEventListener('mouseup', e => {
-    isPainting = false;
-    ctx.stroke();
-    ctx.beginPath();
-});
+// calculate bill per person
+function calcBill() {
+  // get the bill
+  let totalBill = bill.value;
 
-canvas.addEventListener('mousemove', draw);
+  // number of people
+  let totalPeople = people.value;
+  let total = totalBill / totalPeople;
+
+  // tip per person
+  document.querySelector('#amount').value = (total * (tipAmount / 100)).toFixed(2);
+
+  // show total per person
+  document.querySelector('#total').value = total.toFixed(2);
+}
+
+function resetEverything() {
+  location.reload();
+}
+
+people.addEventListener('keyup', calcBill);
+
+custom.addEventListener('keyup', getTipCustom);
+
+reset.addEventListener('click', resetEverything);
